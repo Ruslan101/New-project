15236700834a97ac4f49fc67095a2963b3ef57eb
Ruslan101 <ttracer64@gmail.com>
2019-05-12 23:01:14 +0300

    Add default dir and default css reset and html file

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 
 int main(int argc, char const *argv[])
 {
+	char file[400];
+	char fileReset[7000];
 	mkdir("New_Project", 0755);
 	mkdir("New_Project/js", 0755);
 	mkdir("New_Project/css", 0755);
@@ -11,12 +13,31 @@ int main(int argc, char const *argv[])
 	FILE *fptr_1 = fopen("New_Project/index.html", "a");
 	FILE *fptr_2 = fopen("New_Project/js/script.js", "a");
 	FILE *fptr_3 = fopen("New_Project/css/style.css", "a");
+	FILE *fptr_4 = fopen("New_Project/css/reset.css", "a");
+	FILE *fptr_5 = fopen("default/default.html", "r");
+	FILE *fptr_6 = fopen("default/default.css", "r");
 
-	fputs("<!DOCTYPE html>\n<html lang='en'><head>\n<meta charset='UTF-8' http-equiv='X-UA-Compatible' name='index'>\n<meta name='viewport' content='width=device-width'>\n<title></title>\n<link rel='stylesheet' href='css/style.css'>\n<script src='js/script.js' type='text/javascript' charset='utf-8'></script>\n</head>\n<body>\n\n</body>\n</html>", fptr_1);
+	while(!feof(fptr_5))
+	{
+		fgets(file, 400, fptr_5);
+
+		if(!feof(fptr_5))
+			fprintf(fptr_1, "%s", file);
+	}
+	while(!feof(fptr_6))
+	{
+		fgets(fileReset, 6953, fptr_6);
+
+		if(!feof(fptr_6))
+			fprintf(fptr_4, "%s", fileReset);
+	}
 
 	fclose(fptr_1);
 	fclose(fptr_2);
 	fclose(fptr_3);
+	fclose(fptr_4);
+	fclose(fptr_5);
+	fclose(fptr_6);
 
 	return 0;
 }
